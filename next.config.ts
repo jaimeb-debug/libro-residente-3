1650ebb10d3b0a69e6f7a1ae0f0c509e3b6afc9e
jaimeb-debug <jaimeb1971ia@gmail.com>
2026-04-29 08:02:32 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://www.udz3c.es;"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://wwww.udz3c.es"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
